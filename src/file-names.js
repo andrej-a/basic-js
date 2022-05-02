263751d3b0b8a9e3dc7f92ed4c0c38895d2a75b0
github-usernameandrej-a <amelyanovich.andrej@mail.ru>
2022-05-02 19:18:35 +0300

    feat: add file-names solution

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function renameFiles(names) {
+  let result = [...names];
+  for (let i = 0; i < names.length; i++) {
+      let counter = 0;
+      let next = i+1;
+      while (next <= names.length -1 ){
+          if (result[i] === result[next]) {
+          counter++
+          result[next] = result[next] + '(' + counter + ')'
+      }
+      next++;
+      }
+      
+  }
+  return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+ class VigenereCipheringMachine {
+  constructor(order = true){
+      this.alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  letterToNumber(message, key) {
+      let m = message.replace(/[^A-Z]/gi, "").toUpperCase();//убираем все, кроме букв
+      let k = key.length > m.length ? key.slice(0, m.length).toUpperCase() : key.toUpperCase();//если ключ длиннее -- обрезаем
+      let messageNumbers = [];//массив для цифр сообщения
+      let keyNumber = []; //массив для цифр ключа
+
+      this.alph.map((lett, i) => {
+          for (let j = 0; j < m.length; j++) {
+              if (lett === m[j]) {
+                  messageNumbers.push(i + 1);
+                  console.log(messageNumbers);
+              }
+          }
+      });
+
+      this.alph.map((lett, i) => {
+          for (let j = 0; j < k.length; j++) {
+              if (lett === k[j]) {
+                  keyNumber.push(i + 1);
+              }
+          }
+      });
+
+  }
+
+  encrypt(message, key) {
+      if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    
+  }
+  decrypt(message, key) {
+      if (!message || !key) {
+          throw new Error("Incorrect arguments!");
+      }  
   }
 }
 
